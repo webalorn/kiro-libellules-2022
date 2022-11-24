43897e328f9c691462a3300a0f79173ebdee27c7
Victor Miquel <v_m_1732@orange.fr>
2022-11-24 18:04:19 +0100

    Cautious algo

--- code/main.py
+++ code/main.py
@@ -5,8 +5,7 @@ import victor
 import explorer
 
 def generate_base_solution(in_data):
-    return victor.process_very_basic(in_data)
-    # return explorer.generate_with_exploration(in_data)
+    return victor.process(in_data)
     #return {'empty' : True} # TODO : use functions from modules
 
 def improve_sol(data):
